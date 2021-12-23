706e923120bd2f77f66961fedeb2352b0eae2af6
Muhammad Agus Luthfi <61788600+p4nserb1ru14@users.noreply.github.com>
2021-12-23 08:50:30 +0700

    new

---
+++ app/src/main/java/com/example/lm2/IntroPageActivity.java
@@ -0,0 +1,14 @@
+package com.example.lm2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class IntroPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro_page);
+    }
+}
